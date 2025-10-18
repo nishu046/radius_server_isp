5950945242c9ff1a9f7d8a492a2a55e7c0f18e89
nishu046 <mdasaduzzamanashikm@gmail.com>
2025-10-19 01:46:25 +0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,13 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import importlib
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'isp_management.settings')
     try:
-        from django.core.management import execute_from_command_line
+        execute_from_command_line = importlib.import_module('django.core.management').execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
